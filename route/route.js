23f3a204e74e829fb57f009eca727a57b11c0899
jangxin115 <jangxin115@zoho.com>
2017-11-18 17:37:52 +0000

    Update stock data request

--- route/route.js
+++ route/route.js
@@ -2,35 +2,53 @@ var express = require('express');
 var http = require("http");
 var symbolsDatabase = require("./symbol_database.js");
 var router = express.Router();
-var host = "demo_feed.tradingview.com";
-
+var host = "tvc4.forexpros.com"
+var resData = [];
+var resSearchData = [];
 
 router
     .route('/stockdata')
     .get(function (req, res) {        
-        var resData = [];
-
-        var resolution = req.query.resolution;
-        var dateStart = req.query.from;
-        var dateEnd = req.query.to; 
+        resData = [];
+        resSearchData = [];
         var symbol = req.query.symbol;
         var symbols = symbol.split('%');
-        symbols.forEach(function(element) {
-            var path =  '/history?symbol=' + element + '&resolution=' + resolution + '&from=' + dateStart + '&to=' + dateEnd;
-            var options = {
+        symbols.forEach(function(element) {   
+            var searchpath = '/74b5368ec1066d04ad9d2cc10fd433ed/1511021910/1/1/8/search?limit=60&query='+ element +'&type=&exchange='
+            var searchoptions = {
                 "host": host,
-                "path": path
+                "path": searchpath
             };
-            new HttpGet(options,element,resolution,getCallBack);
-        });       
-
-        function getCallBack(data){            
-            resData.push(data);
-            if (resData.length == symbols.length){
-                res.status(200).json(resData);
+            new HttpGetQuery(searchoptions,element,getSearchCallBack);            
+        });
+        function getSearchCallBack(data){            
+            resSearchData.push(data);
+            if (resSearchData.length == symbols.length){
+                getDataWithQuery(symbols,res);
             }
         }
 });
+
+function getDataWithQuery(symbolsin,res){
+    var d = new Date();
+    d.setDate(d.getDate() - 5);        
+    var dateStart = Math.round(d.getTime() / 1000);
+    var dateEnd = Math.round((new Date()).getTime() / 1000); 
+    symbolsin.forEach(function(element) {
+        var path = '/81957d16c038d3c488ca812fc635f26c/1510943380/1/1/8/history?symbol=' + element + '&resolution=' + '5' + '&from=' + dateStart + '&to=' + dateEnd            
+        var options = {
+            "host": host,
+            "path": path
+        };
+        new HttpGet(options,element,getCallBack);
+    });    
+    function getCallBack(data){            
+        resData.push(data);
+        if (resData.length == symbolsin.length){
+            res.status(200).json(resData);
+        }
+    }
+}
 router
     .route('/stockmarket')
     .get(function (req, res) {
@@ -43,30 +61,48 @@ router
         }
 });
 
-function convertData(datain,symbol,resolution){
+function convertData(datain,symbol){
     var resArr = [];
     var data = parseJSONorNot(datain);
-    for (var i = 0; i < data.t.length ; i ++)
-    {
-        var tmpValue = {
-            T : data.t[i],
-            O : data.o[i],
-            H : data.h[i],
-            L : data.l[i],
-            C : data.c[i],
-            V : data.v[i]
-        };
-        resArr.push(tmpValue);
-        
-    }
+    var tmpValue = {            
+        O : data.o[data.t.length - 1],
+        H : data.h[data.t.length - 1],
+        L : data.l[data.t.length - 1],
+        C : data.c[data.t.length - 1],
+        V : data.v[data.t.length - 1]
+    };
     var resData = {
-        symbol : symbol,
-        resolution : resolution,
-        tickdata : resArr
+        symbol : symbol,        
+        tickdata : tmpValue,
+        description : checkQueryData(symbol)
     }
     return resData;
 }
-function HttpGet(options, symbol, resolution, callback) {
+function checkQueryData(sym){
+    for (var i = 0;i < resSearchData.length; i ++){
+        var tmp = resSearchData[i];
+        if(tmp.symbol == sym){
+            return tmp.description;
+        }
+    }
+    return '';
+}
+function getQeuryData(datain,symbol){
+    var resArr = {};
+    var data = parseJSONorNot(datain);
+    data.forEach(function(row){
+        if (row.symbol == symbol){
+            if (row.exchange .toUpperCase().includes('NYSE')){
+                resArr = row;
+            }else if(row.exchange.toUpperCase().includes('NASDAQ')){
+                resArr = row;
+            }
+        }
+    });
+    return resArr;
+   
+}
+function HttpGet(options, symbol, callback) {
 	function onDataCallback(response) {
 		var result = '';
 		response.on('data', function (chunk) {
@@ -77,7 +113,35 @@ function HttpGet(options, symbol, resolution, callback) {
 				callback('');
 				return;
 			}
-			callback(convertData(result,symbol,resolution));
+			callback(convertData(result,symbol));
+		});
+	}
+    var req = http.request(options, onDataCallback);
+	req.on('socket', function (socket) {
+		socket.setTimeout(5000);
+		socket.on('timeout', function () {
+				req.abort();
+		});
+	});
+
+	req.on('error', function (e) {
+		callback('');
+	});
+	req.end();
+}
+
+function HttpGetQuery(options, symbol, callback) {
+	function onDataCallback(response) {
+		var result = '';
+		response.on('data', function (chunk) {
+			result += chunk;
+		});
+		response.on('end', function () {
+			if (response.statusCode !== 200) {
+				callback('');
+				return;
+			}
+			callback(getQeuryData(result,symbol));
 		});
 	}
     var req = http.request(options, onDataCallback);
